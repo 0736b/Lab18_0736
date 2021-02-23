3a18ec76ba35bf90ed26f4e985481d5b67d64adc
0736b <thanawat_bumpengpun@cmu.ac.th>
2021-02-23 17:40:05 +0700

    lab18 completed

--- lab18_1.cpp
+++ lab18_1.cpp
@@ -1,12 +1,32 @@
 #include<iostream>
+#include<algorithm>
+#include<cmath>
 using namespace std;
 
 struct Rect{
 	double x,y,w,h;
 };
 
-double overlap(){
+double overlap(Rect R1, Rect R2){
+    double distX = 0, distY = 0, XW1, XW2, YH1 , YH2;
+	XW1 = R1.x+R1.w;
+	XW2 = R2.x+R2.w;
+	YH1 = R1.y-R1.h;
+	YH2 = R2.y-R2.h;
 
+	distX = min(XW1,XW2) - max(R1.x,R2.x);
+	distY = min(R1.y,R2.y) - max(YH1,YH2);
+	
+	if(distX > 0 && distY >0){
+		return distX * distY;
+	}
+	else{
+	return 0;
+	}
+}
 
-
+int main(){
+	Rect R1 = {1,1,5,5};
+Rect R2 = {7,2,3,3};	
+cout << overlap(R1,R2);	
 }
--- lab18_2.cpp
+++ lab18_2.cpp
@@ -1,12 +1,26 @@
 #include<iostream>
+#include<algorithm>
+#include<cmath>
 using namespace std;
 
 struct Rect{
 	double x,y,w,h;
 };
 
-double overlap(){
+double overlap(Rect *R1, Rect *R2){
+    double distX = 0, distY = 0, XW1, XW2, YH1 , YH2;
+	XW1 = R1->x + R1->w;
+	XW2 = R2->x + R2->w;
+	YH1 = R1->y - R1->h;
+	YH2 = R2->y - R2->h;
 
-
-
+	distX = min(XW1,XW2) - max(R1->x,R2->x);
+	distY = min(R1->y,R2->y) - max(YH1,YH2);
+	
+	if(distX > 0 && distY >0){
+		return distX * distY;
+	}
+	else{
+	return 0;
+	}
 }
